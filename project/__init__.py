31b5621bee38527ba7e76a95a5333ef524dfa834
Rogerio Prando <rogerio.prando@wplex.com.br>
2018-04-30 13:33:45 -0300

    Added use of instance folder

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,7 @@
 # also, creates the instance of the Flask module
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
 # import after instance,
 from . import views
